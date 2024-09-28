aa26590033ae56c1902c9642ff09badd3e0ed4d0
Hossam Gomah <hossamgomah4@gmail.com>
2024-09-28 06:04:33 -0700

    m

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,38 @@
+task = input("Enter your task ")
+
+priority = input("Priority (high/medium/low): ")
+
+time_bound = input("Is it time-bound? (yes/no): ")
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+
+        elif time_bound == "no":
+                        print(f"Reminder: '{task}' is a high priority task. Consider completing it when you have free time.")
+
+        else:
+               print("pls type yes or no")
+
+
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
+
+        elif time_bound == "no":
+                        print(f"Reminder: '{task}' is a medium priority task. Consider completing it when you have free time.")
+
+        else:
+               print("pls type yes or no")
+
+
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
+
+        elif time_bound == "no":
+                        print(f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time.")
+
+        else:
+               print("pls type yes or no")
