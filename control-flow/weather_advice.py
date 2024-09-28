a223e8bb8c8848d405e0feb9fa1842ac1f77088e
Hossam Gomah <hossamgomah4@gmail.com>
2024-09-27 23:23:06 -0700

    m

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
-guess = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if guess == "sunny":
+if weather == "sunny":
         print("Wear a t-shirt and sunglasses.")
 
-elif guess == "sunny":
+elif weather == "sunny":
         print("Don't forget your umbrella and a raincoat.")    
 
-elif guess == "cold":
+elif weather == "cold":
         print("Make sure to wear a warm coat and a scarf.")
         
 else:
