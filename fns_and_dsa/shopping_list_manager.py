c5a1b6aa17a318a46e4bb27cc6c552b3809d9644
Hossam Gomah <hossamgomah4@gmail.com>
2024-10-07 04:38:21 -0700

    m

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,7 @@
 def display_menu():
     print("Shopping List Manager")
-    print("1. Add an item")
-    print("2. Remove an item")
+    print("1. Add Item")
+    print("2. Remove Item")
     print("3. View the list")
     print("4. Exit")
 
